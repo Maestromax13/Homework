86a28068dcb7029375b08f8780b435ce1abd4b17
Max Yaromchuk <maxmaestro1330@gmail.com>
2019-05-16 17:04:18 +0300

    OOP

--- Rock/add.js
+++ Rock/add.js
@@ -1,61 +1,75 @@
-const getUserChoice = userInput => {
-  userInput = userInput.toLowerCase();
+// {
+// const getUserChoice = userInput => {
 
-  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
-    return userInput;
-  } else {
-    return console.log('Error');
+//   userInput = userInput.toLowerCase();
+
+//   if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+//     return userInput;
+//   } else {
+//     return console.log('Error');
+//   }
+// };
+
+// const getComputerChoice = () => {
+//   let randomNumber = Math.floor(Math.random() * 3);
+//   switch (randomNumber) {
+//     case 0:
+//       return 'rock';
+//       break;
+//     case 1:
+//       return 'paper';
+//       break;
+//     case 2:
+//       return 'scissors';
+//       break;
+//   }
+// };
+
+// const determineWinner = (userChoice, computerChoice) => {
+//   if (userChoice === computerChoice) {
+//     return 'It is a tie';
+//   };
+//   if (userChoice === 'rock') {
+//     if (computerChoice === 'paper') {
+//       return 'The computer won!';
+//     } else {
+//       return 'You won!';
+//     }
+//   };
+//   if (userChoice === 'paper') {
+//     if (computerChoice === 'scissors') {
+//       return 'The computer won!';
+//     } else {
+//       return 'You won!';
+//     }
+//   };
+//   if (userChoice === 'scissors') {
+//     if (computerChoice === 'rock') {
+//       return 'The computer won!';
+//     } else {
+//       return 'You won!';
+//     }
+//   }
+
+// };
+
+// const playGame = () => {
+//   let userChoice = getUserChoice('rock');
+//   let computerChoice = getComputerChoice();
+//   console.log('You threw: ' + userChoice);
+//   console.log('The computer threw: ' + computerChoice);
+//   console.log(determineWinner(userChoice, computerChoice))
+// };
+// playGame();
+// }
+
+const myArray = [1, 2, 3];
+const yourArray = [11, 22, 33];
+for (let i = 0; i < myArray.length; i++) {
+  for (let j = 0; j < yourArray.length; j++) {
+    if (myArray.indexOf(i) % 2 === 1) {
+     myArray.push(yourArray[j]);
+    }
   }
 };
-
-const getComputerChoice = () => {
-  let randomNumber = Math.floor(Math.random() * 3);
-  switch (randomNumber) {
-    case 0:
-      return 'rock';
-      break;
-    case 1:
-      return 'paper';
-      break;
-    case 2:
-      return 'scissors';
-      break;
-  }
-};
-
-const determineWinner = (userChoice, computerChoice) => {
-  if (userChoice === computerChoice) {
-    return 'It is a tie';
-  };
-  if (userChoice === 'rock') {
-    if (computerChoice === 'paper') {
-      return 'The computer won!';
-    } else {
-      return 'You won!';
-    }
-  };
-  if (userChoice === 'paper') {
-    if (computerChoice === 'scissors') {
-      return 'The computer won!';
-    } else {
-      return 'You won!';
-    }
-  };
-  if (userChoice === 'scissors') {
-    if (computerChoice === 'rock') {
-      return 'The computer won!';
-    } else {
-      return 'You won!';
-    }
-  }
-
-};
-
-const playGame = () => {
-  let userChoice = getUserChoice('rock');
-  let computerChoice = getComputerChoice();
-  console.log('You threw: ' + userChoice);
-  console.log('The computer threw: ' + computerChoice);
-  console.log(determineWinner(userChoice, computerChoice))
-};
-playGame();
+console.log(myArray);
